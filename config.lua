e23e450025d146eee0a536bb4a2ba574960edfa4
minhkienoffcial-max <minhkienoffcial@gmail.com>
2025-09-01 16:43:23 +0700

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,80 @@
+return {
+  BUY_TRAVELING_MERCHANT = { "Bee Egg","Bug Egg","Paradise Egg","Loquat","Feijoa","Pitcher Plant" },
+  CLAIM_FAIRY_REWARD     = { "Enchanted Egg","Exotic Sparkle Egg","FairyPoints","Pet Shard Glimmering" },
+
+  AUTO_UPDATE_RESTART = true,
+  REDEEM_CODES = {},
+  EXTRA_PET_SLOTS = 5,
+  EXTRA_EGG_SLOTS = 5,
+  ADD_FRIEND = true,
+  OPEN_ALL_SEED_PACK = true,
+
+  MAX_PLANTS = 220,
+  DESTROY_UNTIL_MIN_PLANTS = 200,
+  DELETE_PLANTS_AFTER_MAX = { "Carrot","Strawberry","Blueberry","Tomato","Apple" },
+
+  BUY_EGGS   = { "Enchanted Egg","Bee Egg","Bug Egg","Paradise Egg","Summer Egg","Nature Egg" },
+  PLANT_EGGS = { "Enchanted Egg","Exotic Sparkle Egg","Bee Egg","Bug Egg","Paradise Egg","Summer Egg","Nature Egg" },
+
+  BUY_SEED_SHOP = {
+    "Ember Lily","Daffodil","Orange Tulip","Burning Bud",
+    "Pepper","Mushroom","Grape",
+    Apple=50,Pumpkin=50,Watermelon=50,Blueberry=50,Strawberry=50,Carrot=50
+  },
+  KEEP_SEEDS = { "Bone Blossom","Orange Tulip","Burning Bud" },
+  KEEP_SEEDS_AFTER_MAX_PLANTS = { "Carrot","Strawberry","Blueberry","Tomato","Apple" },
+
+  FAVOURITE_FRUIT_MUTATIONS = {},
+  SKIP_HARVEST_MUTATIONS = {},
+
+  KEEP_PETS = {
+    "Cockatrice", ["Glimmering Sprite"]=8, "Pixie",
+    "Wasp","Peach Wasp","Tarantula Hawk","Disco Bee","Queen Bee",
+    ["Seal"]=2,
+    "Griffin","Ankylosaurus","Spinosaurus","Brontosaurus","T-Rex",
+    "French Fry Ferret","Lobster Thermidor","Mochi Mouse","Corrupted Kitsune",
+    "Golden Goose","Spriggan","Junkbot","Gorilla Chef","Kitsune","Koi","Bald Eagle","Raccoon","Night Owl","Dragonfly","Butterfly","Mimic Octopus","Red Fox","Hyacinth Macaw",
+    "Pack Bee","Petal Bee","Bear Bee","Hamster","Blood Owl","Cooked Owl","Golden Bee","Owl","Firefly","Chicken Zombie",
+    ["Kodama"]=6,["Corrupted Kodama"]=6,["Tanchozuru"]=5,["Kappa"]=5,
+
+    "Ladybug",
+    ["Blood Kiwi"]=5,
+    ["Chicken"]=4,
+    ["Rooster"]=2,
+    ["Bald Eagle"]=1,
+
+    ["Starfish"]=2, ["Capybara"]=1
+  },
+  KEEP_PETS_WEIGHT = 7,
+  KEEP_PETS_AGE = 90,
+
+  EQUIP_PETS = {
+    ["Ladybug"]=1,
+    ["Pixie"]=1,
+    ["Wasp"]=1,
+    ["Bee"]=1,
+    ["Blood Kiwi"]=2,
+    ["Chicken"]=2,
+    ["Rooster"]=2
+  },
+
+  USE_PETS_FOR_UPGRADE_SLOT = { "Starfish" },
+  REMOVE_PET_MAX_UPGRADE   = { "Starfish","Capybara" },
+
+  BUY_GEAR_SHOP = { "Grandmaster Sprinkler","Master Sprinkler","Godly Sprinkler","Advanced Sprinkler","Basic Sprinkler","Trading Ticket" },
+  USE_SPRINKLER = { "Grandmaster Sprinkler","Master Sprinkler","Godly Sprinkler","Advanced Sprinkler","Basic Sprinkler" },
+
+  PET_WEBHOOK_URL = "",
+  SEED_WEBHOOK_URL = "",
+  NOTIFY_PETS = {
+    "Blood Kiwi","Rooster","Bald Eagle",
+    "Gnome","Griffin","Golden Goose","Spriggan","Gorilla Chef","Lobster Thermidor","French Fry Ferret","Mochi Mouse",
+    "Corrupted Kitsune","Raiju","Mizuchi","Koi","Kitsune","Fennec Fox","Disco Bee","Raccoon","Queen Bee",
+    "Night Owl","Dragonfly","Butterfly","Mimic Octopus","Red Fox","Brontosaurus","T-Rex","Ankylosaurus","Spinosaurus"
+  },
+  NOTIFY_PETS_WEIGHT = 7,
+  DISCORD_ID = "",
+  WEBHOOK_NOTE = "",
+  SHOW_WEBHOOK_USERNAME = true,
+  SHOW_WEBHOOK_JOBID = true
+}
